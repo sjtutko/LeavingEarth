a9e947b1b42dc8d2cdab753fd65e57b2c486b91a
Sam Tutko <sjtutko@gmail.com>
2019-03-15 18:32:35 -0700

    initial code commit, file names and headers

---
+++ gameboards.py
@@ -0,0 +1,26 @@
+"""Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+
+--------------------------gameboards.py------------------------------
+This module represents the game boards and missions of Leaving Earth 
+(by the Lumenaris Group) and the two expansion packs: Outer Planets 
+and Stations.
+
+This module is not intended as a stand-alone module and has no main()
+This module contains the Gameboard class, which is initialized with 
+the maps of each game board.
+"""
---
+++ leavingEarth.py
@@ -0,0 +1,24 @@
+"""Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+
+----------------------------leavingEarth.py-------------------------------
+This module simulates a game of Leaving Earth (by the Lumenaris Group).
+This module is intended to be run as a play-along companion or
+strategic planning tool.  It contains a Nation class as well as methods
+for asset and game-piece tracking, strategy, and game simulation.
+This module requires gameboards.py, technologies.py, and missionPlanner.py
+"""
