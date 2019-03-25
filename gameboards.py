67f26d36d959e77de2a8dcfa60d5032dc25106d9
Sam Tutko <sjtutko@gmail.com>
2019-03-24 21:19:58 -0700

    determined data structure, started entering map

--- gameboards.py
+++ gameboards.py
@@ -24,3 +24,46 @@ This module is not intended as a stand-alone module and has no main()
 This module contains the Gameboard class, which is initialized with 
 the maps of each game board.
 """
+
+"""Gameboards are held in a dictionary object.
+Keys are nodes, Values are Edges
+Edges are tuples of the form (destination, cost, time, [hazards])"""
+
+gameboard = {'Earth' : [('Sub-orbital Space',3,0,[]),
+                        ('Earth Orbit',8,0,[])],
+             'Sub-orbital Space' : [('Earth',-1,0,[]),
+                                    ('Earth Orbit',5,0,[])],
+             'Earth Orbit' : [('Earth', 0, 0, ['atmosphere']),
+                              ('Lunar Fly-by', 1, 0, []),
+                              ('Lunar Orbit', 3, 0, []),
+                              ('Mars Fly-by', 3, 3, ['radiation']),
+                              ('Mars Orbit', 5, 3, ['radiation']),
+                              ('Inner Planetary Transfer', 3, 1, []),
+                              ('Outer Planetary Transfer', 6, 1, ['radiation'])],
+             'Lunar Fly-by' : [('Earth Orbit', 1, 0, []),
+                               ('Lunar Orbit', 2, 0, []),
+                               ('Moon', 4, 0, ['landing']), 
+                               ('LOST', -1, 0, [])],
+             'Lunar Orbit' : [('Earth Orbit', 3, 0, []),
+                              ('Moon', 2, 0, ['landing'])],
+             'Moon' : [('Lunar Orbit', 2, 0, [])],
+             'Mars Fly-by' : [('Mars Orbit', 3, 1, []),
+                              ('Mars', 3, 0, ['atmosphere','landing']),
+                              ('LOST', -1, 0, [])],
+             'Mars Orbit' : [('Earth Orbit', 5, 3, ['radiation']),
+                             ('Mars', 0, 0, ['atmosphere', 'landing']),
+                             ('Phobos', 1, 0, ['landing']),
+                             ('Inner Planetary Transfer', 4, 2, ['radiation']),
+                             ('Outer Planetary Transfer', 5, 1, ['radiation'])],
+             'Mars' : [('Mars Orbit', 3, 0, [])],
+             'Phobos' : [('Mars Orbit', 1, 0, [])],
+             'Ceres' : [], 
+             'Venus Fly-by' : [],
+             'Venus Orbit' : [],
+             'Venus' : [],
+             'Mercury Fly-by' : [],
+             'Mercury Orbit' : [],
+             'Mercury' : [],
+             'Inner Planetary Transfer' : [],
+             'Outer Planetary Transfer' : [],
+             'LOST' : []}
