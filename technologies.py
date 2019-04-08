2034f48decb9325b630ace1dd06de245258f3e6e
Sam Tutko <sjtutko@gmail.com>
2019-04-07 21:37:33 -0700

    added spacecraft definitions first version of attemptManeuver

--- technologies.py
+++ technologies.py
@@ -23,6 +23,38 @@ and Stations.
 This module is not intended as a stand-alone module and has no main()
 This module contains several classes, which record the attributes of
 each technology or vehicle in the game.
+
+Class Hierarchy and Attributes:
+|Item
+|*cost
+|+Payload
+||*mass
+||*capabilities
+||*restrictions
+||+Astronaut
+|||*skill
+|||*name
+|||*status
+||+RockSample
+|||*origin
+||+Supplies
+||+Craft
+|||*capacity
+|||*status
+|||*hazardousManeuvers
+|||+Probe
+|||+Eagle
+|||+Vostok
+|||+Apollo
+|||+Aldrin
+|+Advancement
+||*riskCards
+||*capability
+||+Surveying
+||+Rendezvous
+||+LifeSupport
+||+ReEntry
+||+Landing
 """
 from abc import ABC, abstractmethod
 from random import choice, sample
@@ -61,7 +93,8 @@ class payload(item):
 class astronaut(payload):
   """defining class for all atronauts.
   astronaut skills: medic, pilot,
-  all astronauts have zero mass (stated for clarity)"""
+  all astronauts have zero mass (stated for clarity)
+  status options: healthy, incapacitated, dead"""
   
   _names = {'medic' : ['Mike Collins','Valentina Tereshkova','Valery Bykovsky',
                        'Gherman Titov', 'Boris Yegorov'],
@@ -74,6 +107,7 @@ class astronaut(payload):
     super().__init__()
     self._cost = 5
     self._skill = skill
+    self._status = 'healthy'
     if name is not '':
       self._name = name
     else:
@@ -84,6 +118,37 @@ class astronaut(payload):
   
   def getName(self):
     return self._name
+
+  def assignName(self, name = ''):
+    if name is not '':
+      self._name = name
+    else:
+      self._name = choice(astronaut._names[self._skill])
+    return self._name
+  
+  def getStatus(self):
+    return self._status
+
+  def injure(self):
+    self._status = 'incapacitated'
+
+  def kill(self):
+    self._status = 'dead'
+
+  def heal(self, crewSkills = [], location = 'away'):
+    if self._status == 'healthy':
+      print(self._name + " is healthy")
+    elif self._status == 'incapacitated':
+      if 'medic' in crewSkills:
+        self._status = 'healthy'
+        print(self._name + " is now healthy")
+      elif location == 'Earth':
+        self._status = 'healthy'
+        print(self._name + " is now healthy")
+      else:
+        print(self._name + " cannot be healed")
+    else:
+      print(self._name + " is dead and cannot be healed")
 
 class advancement(item):
   """parent class for all advancements"""
@@ -103,7 +168,13 @@ class advancement(item):
     self.riskCards = sample(advancement._riskDeck,3)
   
   def getRisk(self):
-    return choice(self.riskCards)
+    if len(self.riskCards):
+      return choice(self.riskCards)
+    else:
+      return 'success'
+  
+  def buyDownRisk(self, riskOutcome):
+    self.riskCards.remove(riskOutcome)
 
   @staticmethod
   @abstractmethod
@@ -139,7 +210,7 @@ class rendezvous(advancement):
       elif riskOutcome == 'minor failure' and 'pilot' in crewSkills:
         return "Docking/Separating Successful"
       else:
-        return "No Docking/Separating, Damage Chosen Comp."
+        return "No Docking/Separating, damage chosen comp."
 
 class lifeSupport(advancement):
   @staticmethod
@@ -151,9 +222,9 @@ class lifeSupport(advancement):
     if riskOutcome == 'success':
       return "Occupants Survive"
     elif riskOutcome == 'minor failure' and 'mechanic' in crewSkills:
-        return "Occupants Survive"
+        return "Occupants survive"
     else:
-      return "Occupants Die"
+      return "Occupants die"
 
 class reEntry(advancement):
   @staticmethod
@@ -165,9 +236,9 @@ class reEntry(advancement):
     if riskOutcome == 'success':
       return "Atmospheric Re-Entry Successful"
     elif riskOutcome == 'minor failure':
-      return "Capsule is damaged, Occupants Survive"
+      return "Capsule is damaged, occupants survive"
     elif riskOutcome == 'major failure':
-      return "Capsule is destroyed, Occupants Die"
+      return "Capsule is destroyed, occupants die"
 
 class landing(advancement):
   @staticmethod
@@ -181,11 +252,11 @@ class landing(advancement):
     elif riskOutcome == 'minor failure' and 'pilot' in crewSkills:
       return "Landing Successful"
     elif riskOutcome == 'minor failure':
-      return "Rough Landing, Damage Chosen Comp."
+      return "Rough landing, damage chosen comp."
     elif riskOutcome == 'major failure' and 'pilot' in crewSkills:
-      return "Rough Landing, Damage Chosen Comp."
+      return "Rough landing, damage chosen comp."
     elif riskOutcome == 'major failure':
-      return "Impact with Surface, Spacecraft Destroyed"
+      return "Impact with surface, spacecraft destroyed"
 
 class rockSample(payload):
   def __init__(self, origin):
@@ -204,53 +275,83 @@ class supplies(payload):
     self._mass = quantity
     self._restrictions = ['life support']
 
-class probe(payload):
-  def __init__(self):
-    super().__init__()
-    self._cost = 2
-    self._mass = 1
-    self._capabilities = ['surveying','rendezvous']
-    self._restrictions = []
-    self._status = 'operational'
+class craft(payload):
+  """craft can be either operational, damaged, or destroyed
+  craft can be repaired, by default if on earth
+  hazardous maneuvers are maneuvers that can damage the craft"""
 
-  def getStatus(self):
-    return self._status
-
-  def damage(self):
-    self._status = 'damaged'
-
-  def repair(self, crewSkills = [], location = 'away'):
-    if 'mechanic' in crewSkills:
-      self._status = 'operational'
-    elif location == 'Earth':
-      self._status = 'operational'
-    else:
-      print("probe cannot be repaired")
-
-class capsule(payload):
   def __init__(self):
     super().__init__()
     self._capacity = 0
     self._status = 'operational'
+    self._capabilities = set([])
+    self._restrictions = set([])
+    self._hazardousManeuvers = set([])
 
   def getCapacity(self):
     return self._capacity
+
+  def getHazMan(self):
+    return self._hazardousManeuvers
   
   def getStatus(self):
     return self._status
 
   def damage(self):
     self._status = 'damaged'
+    print("craft damaged")
+
+  def destroy(self):
+    self._status = 'destroyed'
+    print("craft destroyed")
+
+  def attemptManeuver(self, maneuver, advancements, crewSkills = []):
+    """technologies is an dict of advancement instances where the advancement
+    name is the key of the dict"""
+    if maneuver in self._hazardousManeuvers:
+      self.destroy()
+    elif self._restrictions.union([maneuver]) <= set(advancements.keys()):
+      if maneuver in self._capabilities:
+        if self._status == 'operational':
+          result =  advancements[maneuver].getRisk()
+        else:
+          print("spacecraft is not operational")
+          result = 'major failure'
+        outcome = advancements[maneuver].getOutcome(result, crewSkills)
+        if 'damage' in outcome:
+          self.damage()
+        return outcome
+      else:
+        print("craft cannot perform that maneuver")
+    else:
+      print("the advancement for this maneuver has not been researched")
 
   def repair(self, crewSkills = [], location = 'away'):
-    if 'mechanic' in crewSkills:
-      self._status = 'operational'
-    elif location == 'Earth':
-      self._status = 'operational'
+    if self._status == 'operational':
+      print("craft is operational")
+    elif self._status == 'damaged':
+      if 'mechanic' in crewSkills:
+        self._status = 'operational'
+        print("craft repaired")
+      elif location == 'Earth':
+        self._status = 'operational'
+        print("craft repaired")
+      else:
+        print("craft cannot be repaired")
     else:
-      print("capsule cannot be repaired")
+      print("craft cannot be repaired")
 
-class eagle(capsule):
+class probe(craft):
+  def __init__(self):
+    super().__init__()
+    self._capacity = 0
+    self._cost = 2
+    self._mass = 1
+    self._capabilities = set(['surveying','rendezvous','radiation'])
+    self._restrictions = set([])
+    self._hazardousManeuvers = set(['re-entry', 'landing'])
+
+class eagle(craft):
   def __init__(self):
     super().__init__()
     self._capacity = 2
@@ -258,3 +359,34 @@ class eagle(capsule):
     self._cost = 4
     self._capabilities = ['surveying','rendezvous','landing','life support']
     self._restrictions = ['landing']
+    self._hazardousManeuvers = ['re-entry','radiation']
+
+class vostok(craft):
+  def __init__(self):
+    super().__init__()
+    self._capacity = 1
+    self._mass = 2
+    self._cost = 2
+    self._capabilities = ['surveying','rendezvous','life support','re-entry']
+    self._restrictions = ['re-entry']
+    self._hazardousManeuvers = ['landing','radiation']
+
+class apollo(craft):
+  def __init__(self):
+    super().__init__()
+    self._capacity = 3
+    self._mass = 3
+    self._cost = 4
+    self._capabilities = ['surveying','rendezvous','life support','re-entry']
+    self._restrictions = ['re-entry']
+    self._hazardousManeuvers = ['landing','radiation']
+
+class aldrin(craft):
+  def __init__(self):
+    super().__init__()
+    self._capacity = 8
+    self._mass = 3
+    self._cost = 4
+    self._capabilities = ['surveying','rendezvous','life support','radiation']
+    self._restrictions = ['life support']
+    self._hazardousManeuvers = ['re-entry','landing']
